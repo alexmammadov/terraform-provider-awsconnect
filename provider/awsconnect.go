225541a97f04221fa4e9c6a01ae8f7264bd322fb
alexmammadov <alexmammadov76@gmail.com>
2021-08-31 10:40:27 -0700

    support for aws container creds

--- provider/awsconnect.go
+++ provider/awsconnect.go
@@ -4,6 +4,8 @@ import (
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/defaults"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/connect"
 )
@@ -20,25 +22,25 @@ import (
 // 	return connect.New(sess)
 // }
 
-// func connectService() *connect.Connect {
-// 	cfg := defaults.Config()
-// 	handlers := defaults.Handlers()
-
-// 	p := defaults.RemoteCredProvider(*cfg, handlers)
-
-// 	sess := session.Must(session.NewSession(&aws.Config{
-// 		Region:      aws.String(os.Getenv("AWS_REGION")),
-// 		Credentials: credentials.NewCredentials(p),
-// 	}))
-
-// 	return connect.New(sess)
-// }
-
 func connectService() *connect.Connect {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Config:            aws.Config{Region: aws.String(os.Getenv("AWS_REGION"))},
-		SharedConfigState: session.SharedConfigEnable,
+	cfg := defaults.Config()
+	handlers := defaults.Handlers()
+
+	p := defaults.RemoteCredProvider(*cfg, handlers)
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:      aws.String(os.Getenv("AWS_REGION")),
+		Credentials: credentials.NewCredentials(p),
 	}))
 
 	return connect.New(sess)
 }
+
+// func connectService() *connect.Connect {
+// 	sess := session.Must(session.NewSessionWithOptions(session.Options{
+// 		Config:            aws.Config{Region: aws.String(os.Getenv("AWS_REGION"))},
+// 		SharedConfigState: session.SharedConfigEnable,
+// 	}))
+
+// 	return connect.New(sess)
+// }
